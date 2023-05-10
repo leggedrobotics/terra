a59e9d0bbabd608c07534aad5f33ca9aab224d99
Antonio Arbues <avarbues@gmail.com>
2023-05-10 15:20:54 +0000

    rewards

--- terra/config.py
+++ terra/config.py
@@ -101,24 +101,24 @@ class AgentConfig(NamedTuple):
 class Rewards(NamedTuple):
     existence: Float = -0.05
 
-    collision_move: Float = -0.2
-    move_while_loaded: Float = -0.1
+    collision_move: Float = -0.01
+    move_while_loaded: Float = -0.05
     move: Float = 0.0
 
-    collision_turn: Float = -0.2
+    collision_turn: Float = -0.01
     base_turn: Float = 0.0
 
     cabin_turn: Float = 0.0
 
     dig_wrong: Float = (
-        -0.05
+        -0.02
     )  # given both if loaded stayed the same, or if new map is not closer than old to target
-    dump_wrong: Float = -0.05  # given if loaded stayed the same
+    dump_wrong: Float = 0.0  # given if loaded stayed the same
 
-    dig_correct: Float = 2.0  # given if the new map is closer to target map than before
-    dump_correct: Float = 0.05  # implemented as dump where not digged
+    dig_correct: Float = 0.15  # given if the new map is closer to target map than before
+    dump_correct: Float = 0.0  # implemented as dump where not digged
 
-    terminal: Float = 10.0  # given if the action map is the same as the target map where it matters (digged tiles)
+    terminal: Float = 5.0  # given if the action map is the same as the target map where it matters (digged tiles)
 
 
 class EnvConfig(NamedTuple):
