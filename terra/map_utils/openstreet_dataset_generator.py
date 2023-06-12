8bc3439e779b884d508b0e0979da6cbded415fc9
Antonio Arbues <avarbues@gmail.com>
2023-06-12 22:19:58 +0200

    dump imgs to npy and test

--- terra/map_utils/openstreet_dataset_generator.py
+++ terra/map_utils/openstreet_dataset_generator.py
@@ -28,7 +28,49 @@ def _get_building_h_w(idx: int) -> tuple[float, float]:
     return w1, h1
 
 
-def generate_openstreet_2(wm, hm: int, div: int):
+def _convert_img_to_terra(img):
+    """
+    Converts an image from [0, 255] convention
+    to [-1, 0, 1] convention.
+    """
+    img = img[..., 0].astype(np.int16)  # squeeze to 1 channel
+    img = np.where(img == 0, -1, img)
+    img = np.where(img == 100, 1, img)
+    img = np.where(img == 255, 0, img)
+    return img.astype(np.int8)
+
+
+def _handle_options(img, option: int, img_path: str | None):
+    """
+    The input image is a NxMx3 uint8 image, where:
+    0 --> dig
+    100 --> dump
+    255 --> nothing
+
+    option: [1, 2]
+    1 --> save image at path (converted to [-1, 0, 1])
+    2 --> show img
+    """
+    if option == 1:
+        img = _convert_img_to_terra(img)
+        np.save(img_path, img)
+    elif option == 2:
+        img = img.astype(np.uint8).repeat(10, 0).repeat(10, 1)
+        cv2.imshow("buildings", img.astype(np.uint8))
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    else:
+        raise RuntimeError(f"Option {option} doesn't exist.")
+
+
+def generate_openstreet_2(
+    wm, hm: int, div: int, option: int = 1, max_n_imgs: int | None = None
+):
+    """
+    option: [1, 2]
+    1 --> save image at path
+    2 --> show imgs
+    """
     w, h = div * wm, div * hm
 
     idx_list = _filter_buildings_on_dims(40, 40)
@@ -42,6 +84,9 @@ def generate_openstreet_2(wm, hm: int, div: int):
 
     n_overlapping = 0
     for i in range(n_combinations):
+        if max_n_imgs is not None and i >= max_n_imgs:
+            break
+
         idx1 = idx_list[0, i]
         idx2 = idx_list[1, i]
 
@@ -67,7 +112,9 @@ def generate_openstreet_2(wm, hm: int, div: int):
         for _ in range(40):
             x1 = np.random.randint(0, w - pic1.shape[0])
             y1 = np.random.randint(0, h - pic1.shape[1])
-            p[x1 : x1 + pic1.shape[0], y1 : y1 + pic1.shape[1]] = pic1
+            p[x1 : x1 + pic1.shape[0], y1 : y1 + pic1.shape[1]] = np.where(
+                pic1 < 255, 0, pic1
+            )
             mask1 = p == 0
 
             x2 = np.random.randint(0, w - pic2.shape[0])
@@ -76,38 +123,39 @@ def generate_openstreet_2(wm, hm: int, div: int):
             mask2 = p1 == 0
             overlapping = np.any(mask1 * mask2)
             if not overlapping:
-                p[x2 : x2 + pic2.shape[0], y2 : y2 + pic2.shape[1]] = pic2
+                p[x2 : x2 + pic2.shape[0], y2 : y2 + pic2.shape[1]] = np.where(
+                    pic2 < 255, 100, pic2
+                )
                 break
 
         if not overlapping:
             # pd = cv2.resize(p, (40, 40)).repeat(10, 0).repeat(10, 1)
-            pd = (
-                skimage.measure.block_reduce(
-                    p, (p.shape[0] // wm, p.shape[1] // hm, 1), np.min
-                )
-                .astype(np.uint8)
-                .repeat(10, 0)
-                .repeat(10, 1)
+            pd = skimage.measure.block_reduce(
+                p, (p.shape[0] // wm, p.shape[1] // hm, 1), np.min
             )
 
-            pd = np.where(pd == 255, 255, 0).astype(np.uint8)
-            p_grey = np.where(p < 255, 100, p).astype(np.uint8)
-            # p += pic1 + pic2
-
-            # cv2.imshow("buildings", p)
-            # cv2.imshow("building downsampled", pd)
-            cv2.imshow(
-                "building comparison", np.where(p_grey == 100, 100, pd).astype(np.uint8)
+            _handle_options(
+                pd,
+                option,
+                f"/home/antonio/Downloads/img_generator/2_buildings/img_{i}.npy",
             )
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+
             continue
         else:
             n_overlapping += 1
             print(f"{n_overlapping=}")
 
+    print(f"Generated {i + 1} images.")
 
-def generate_openstreet_3(wm, hm: int, div: int):
+
+def generate_openstreet_3(
+    wm, hm: int, div: int, option: int = 1, max_n_imgs: int | None = None
+):
+    """
+    option: [1, 2]
+    1 --> save image at path
+    2 --> show imgs
+    """
     w, h = div * wm, div * hm
 
     idx_list = _filter_buildings_on_dims(40, 40)
@@ -118,12 +166,18 @@ def generate_openstreet_3(wm, hm: int, div: int):
     idx_list_b = idx_list[:, None].repeat(n, 1).reshape(-1)
     idx_list = np.vstack([idx_list_a, idx_list_b])  # 2x(N^2) -> all combinations of 2
 
+    img_idx = 0
     for idx3 in idx_list_plain.tolist():
+        if max_n_imgs is not None and img_idx >= max_n_imgs:
+            break
         np.random.shuffle(idx_list.swapaxes(0, 1))
         n_combinations = idx_list.shape[-1]
 
         n_overlapping = 0
         for i in range(n_combinations):
+            if max_n_imgs is not None and img_idx >= max_n_imgs:
+                break
+
             idx1 = idx_list[0, i]
             idx2 = idx_list[1, i]
 
@@ -150,13 +204,15 @@ def generate_openstreet_3(wm, hm: int, div: int):
             for _ in range(40):
                 x1 = np.random.randint(0, w - pic1.shape[0])
                 y1 = np.random.randint(0, h - pic1.shape[1])
-                p[x1 : x1 + pic1.shape[0], y1 : y1 + pic1.shape[1]] = pic1
-                mask1 = p == 0
+                p[x1 : x1 + pic1.shape[0], y1 : y1 + pic1.shape[1]] = np.where(
+                    pic1 < 255, 0, pic1
+                )
+                mask1 = p < 255
 
                 x2 = np.random.randint(0, w - pic2.shape[0])
                 y2 = np.random.randint(0, h - pic2.shape[1])
                 p1[x2 : x2 + pic2.shape[0], y2 : y2 + pic2.shape[1]] = pic2
-                mask2 = p1 == 0
+                mask2 = p1 < 255
 
                 overlapping = np.any(mask1 * mask2)
                 overlapping2 = True
@@ -164,45 +220,42 @@ def generate_openstreet_3(wm, hm: int, div: int):
                     x3 = np.random.randint(0, w - pic3.shape[0])
                     y3 = np.random.randint(0, h - pic3.shape[1])
                     p2[x3 : x3 + pic3.shape[0], y3 : y3 + pic3.shape[1]] = pic3
-                    mask3 = p2 == 0
+                    mask3 = p2 < 255
 
                     overlapping2 = np.any(mask1 * mask2 * mask3)
                     if not overlapping2:
-                        p[x2 : x2 + pic2.shape[0], y2 : y2 + pic2.shape[1]] = pic2
-                        p[x3 : x3 + pic3.shape[0], y3 : y3 + pic3.shape[1]] = pic3
+                        p[x2 : x2 + pic2.shape[0], y2 : y2 + pic2.shape[1]] = np.where(
+                            pic2 < 255, 100, pic2
+                        )
+                        p[x3 : x3 + pic3.shape[0], y3 : y3 + pic3.shape[1]] = np.where(
+                            pic3 < 255, 0, pic3
+                        )
                         break
 
             if not overlapping2:
                 # pd = cv2.resize(p, (40, 40)).repeat(10, 0).repeat(10, 1)
-                pd = (
-                    skimage.measure.block_reduce(
-                        p, (p.shape[0] // wm, p.shape[1] // hm, 1), np.min
-                    )
-                    .astype(np.uint8)
-                    .repeat(10, 0)
-                    .repeat(10, 1)
+                pd = skimage.measure.block_reduce(
+                    p, (p.shape[0] // wm, p.shape[1] // hm, 1), np.min
                 )
 
-                pd = np.where(pd == 255, 255, 0).astype(np.uint8)
-                p_grey = np.where(p < 255, 100, p).astype(np.uint8)
-                # p += pic1 + pic2
+                # p_grey = np.where(p < 255, 100, p).astype(np.uint8)
 
-                # cv2.imshow("buildings", p)
-                # cv2.imshow("building downsampled", pd)
-                cv2.imshow(
-                    "building comparison",
-                    np.where(p_grey == 100, 100, pd).astype(np.uint8),
+                _handle_options(
+                    pd,
+                    option,
+                    f"/home/antonio/Downloads/img_generator/3_buildings/img_{img_idx}.npy",
                 )
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
+                img_idx += 1
                 continue
             else:
                 n_overlapping += 1
                 print(f"{n_overlapping=}")
+
+    print(f"Generated {img_idx + 1} images.")
 
 
 if __name__ == "__main__":
     # TODO implement tile size
     wm, hm = 60, 60  # meters
     div = 10
-    generate_openstreet_3(wm, hm, div)
+    generate_openstreet_3(wm, hm, div, option=1, max_n_imgs=5)
