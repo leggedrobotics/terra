ba4675968cc7103ac9f717eca2071124f9e0aed1
Antonio Arbues <avarbues@gmail.com>
2023-07-30 01:19:12 +0200

    sparse rewards include negative

--- terra/config.py
+++ terra/config.py
@@ -198,20 +198,20 @@ class Rewards(NamedTuple):
     @staticmethod
     def sparse():
         return Rewards(
-            existence=0.0,
-            collision_move=0.0,
+            existence=-0.01,
+            collision_move=-0.1,
             move_while_loaded=0.0,
-            move=0.0,
-            collision_turn=0.0,
-            base_turn=0.0,
-            cabin_turn=0.0,
-            dig_wrong=0.0,
-            dump_wrong=0.0,
-            dump_no_dump_area=0.0,
+            move=-0.05,
+            collision_turn=-0.1,
+            base_turn=-0.1,
+            cabin_turn=-0.02,
+            dig_wrong=-0.3,
+            dump_wrong=-0.3,
+            dump_no_dump_area=-0.02,
             dig_correct=0.0,
             dump_correct=0.0,
             terminal=20.0,
-            force_reset=0.0,
+            force_reset=-2.0,
         )
 
 
