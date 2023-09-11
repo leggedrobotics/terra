a7da4b6b121db3df0aa981d96cd80cca68102599
Antonio Arbues <avarbues@gmail.com>
2023-09-11 11:14:39 +0200

    introduce action masking in main manual

--- viz/main_manual.py
+++ viz/main_manual.py
@@ -15,9 +15,17 @@ def redraw():
 
 
 def key_handler(event):
-    global states, action_type, obs, n_envs, key_maps_buffer, info, i
+    global states, action_type, obs, n_envs, key_maps_buffer, info, i, action_mask, ACTIVATE_ACTION_MASKING
     i += 1
     print(f"step {i}")
+    
+    def apply_action_mask(action):
+        if ACTIVATE_ACTION_MASKING:
+            action_item = action.action.item()
+            if action_mask[0][action_item].item() == False:
+                action = action_type.do_nothing()
+                print(f"MASKED OUT: action {action_item}.")
+        return action
 
     def repeat_action(action, n_times=n_envs):
         return action_type.new(action.action[None].repeat(n_times, 0))
@@ -28,133 +36,172 @@ def key_handler(event):
         env.window.close()
 
     if event.key == "left":
+        action = action_type.anticlock()
+        action = apply_action_mask(action)
         states, (obs, reward, done, info), key_maps_buffer = env.step(
             states,
-            repeat_action(action_type.anticlock()),
+            repeat_action(action),
             env_cfgs,
             key_maps_buffer,
             jnp.zeros((n_envs,), dtype=jnp.bool_),
         )
+        action_mask = info["action_mask"]
         parse_step(states, reward, done, info)
 
     if event.key == "right":
+        action = action_type.clock()
+        action = apply_action_mask(action)
         states, (obs, reward, done, info), key_maps_buffer = env.step(
             states,
-            repeat_action(action_type.clock()),
+            repeat_action(action),
             env_cfgs,
             key_maps_buffer,
             jnp.zeros((n_envs,), dtype=jnp.bool_),
         )
+        action_mask = info["action_mask"]
         parse_step(states, reward, done, info)
 
     if event.key == "up":
+        action = action_type.forward()
+        action = apply_action_mask(action)
         states, (obs, reward, done, info), key_maps_buffer = env.step(
             states,
-            repeat_action(action_type.forward()),
+            repeat_action(action),
             env_cfgs,
             key_maps_buffer,
             jnp.zeros((n_envs,), dtype=jnp.bool_),
         )
+        action_mask = info["action_mask"]
         parse_step(states, reward, done, info)
 
     if event.key == "down":
+        action = action_type.backward()
+        action = apply_action_mask(action)
         states, (obs, reward, done, info), key_maps_buffer = env.step(
             states,
-            repeat_action(action_type.backward()),
+            repeat_action(action),
             env_cfgs,
             key_maps_buffer,
             jnp.zeros((n_envs,), dtype=jnp.bool_),
         )
+        action_mask = info["action_mask"]
         parse_step(states, reward, done, info)
 
     if event.key == "a":
+        action = action_type.cabin_anticlock()
+        action = apply_action_mask(action)
         states, (obs, reward, done, info), key_maps_buffer = env.step(
             states,
-            repeat_action(action_type.cabin_anticlock()),
+            repeat_action(action),
             env_cfgs,
             key_maps_buffer,
             jnp.zeros((n_envs,), dtype=jnp.bool_),
         )
+        action_mask = info["action_mask"]
         parse_step(states, reward, done, info)
 
     if event.key == "d":
+        action = action_type.cabin_clock()
+        action = apply_action_mask(action)
         states, (obs, reward, done, info), key_maps_buffer = env.step(
             states,
-            repeat_action(action_type.cabin_clock()),
+            repeat_action(action),
             env_cfgs,
             key_maps_buffer,
             jnp.zeros((n_envs,), dtype=jnp.bool_),
         )
+        action_mask = info["action_mask"]
         parse_step(states, reward, done, info)
 
     if event.key == "e":
+        action = action_type.extend_arm()
+        action = apply_action_mask(action)
         states, (obs, reward, done, info), key_maps_buffer = env.step(
             states,
-            repeat_action(action_type.extend_arm()),
+            repeat_action(action),
             env_cfgs,
             key_maps_buffer,
             jnp.zeros((n_envs,), dtype=jnp.bool_),
         )
+        action_mask = info["action_mask"]
         parse_step(states, reward, done, info)
 
     if event.key == "r":
+        action = action_type.retract_arm()
+        action = apply_action_mask(action)
         states, (obs, reward, done, info), key_maps_buffer = env.step(
             states,
-            repeat_action(action_type.retract_arm()),
+            repeat_action(action),
             env_cfgs,
             key_maps_buffer,
             jnp.zeros((n_envs,), dtype=jnp.bool_),
         )
+        action_mask = info["action_mask"]
         parse_step(states, reward, done, info)
 
     if event.key == "o":
+        action = action_type.clock_forward()
+        action = apply_action_mask(action)
         states, (obs, reward, done, info), key_maps_buffer = env.step(
             states,
-            repeat_action(action_type.clock_forward()),
+            repeat_action(action),
             env_cfgs,
             key_maps_buffer,
             jnp.zeros((n_envs,), dtype=jnp.bool_),
         )
+        action_mask = info["action_mask"]
         parse_step(states, reward, done, info)
 
     if event.key == "k":
+        action = action_type.clock_backward()
+        action = apply_action_mask(action)
         states, (obs, reward, done, info), key_maps_buffer = env.step(
             states,
-            repeat_action(action_type.clock_backward()),
+            repeat_action(action),
             env_cfgs,
             key_maps_buffer,
             jnp.zeros((n_envs,), dtype=jnp.bool_),
         )
+        action_mask = info["action_mask"]
         parse_step(states, reward, done, info)
 
     if event.key == "i":
+        action = action_type.anticlock_forward()
+        action = apply_action_mask(action)
         states, (obs, reward, done, info), key_maps_buffer = env.step(
             states,
-            repeat_action(action_type.anticlock_forward()),
+            repeat_action(action),
             env_cfgs,
             key_maps_buffer,
             jnp.zeros((n_envs,), dtype=jnp.bool_),
         )
+        action_mask = info["action_mask"]
         parse_step(states, reward, done, info)
 
     if event.key == "l":
+        action = action_type.anticlock_backward()
+        action = apply_action_mask(action)
         states, (obs, reward, done, info), key_maps_buffer = env.step(
             states,
-            repeat_action(action_type.anticlock_backward()),
+            repeat_action(action),
             env_cfgs,
             key_maps_buffer,
             jnp.zeros((n_envs,), dtype=jnp.bool_),
         )
+        action_mask = info["action_mask"]
         parse_step(states, reward, done, info)
 
     if event.key == " ":
+        action = action_type.do()
+        action = apply_action_mask(action)
         states, (obs, reward, done, info), key_maps_buffer = env.step(
             states,
-            repeat_action(action_type.do()),
+            repeat_action(action),
             env_cfgs,
             key_maps_buffer,
             jnp.zeros((n_envs,), dtype=jnp.bool_),
         )
+        action_mask = info["action_mask"]
         parse_step(states, reward, done, info)
 
 
@@ -163,6 +210,8 @@ def parse_step(obs, reward, done, info):
     print("Reward :", reward)
     print("Done: ", done)
 
+
+ACTIVATE_ACTION_MASKING = True
 
 batch_cfg = BatchConfig()
 action_type = batch_cfg.action_type
@@ -182,6 +231,7 @@ states, (obs, rewards, dones, info), key_maps_buffer = env.step(
     key_maps_buffer,
     jnp.zeros((n_envs,), dtype=jnp.bool_),
 )
+action_mask = info["action_mask"]
 i = 0
 env.terra_env.render_obs(
     obs, key_handler=key_handler, block=True, tile_size=tile_size, info=info
