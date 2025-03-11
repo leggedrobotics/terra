a8c6acdc23aaa9f91f679e74ddd4f39d606e77e3
Stachq1 <spiasecki@student.ethz.ch>
2025-03-11 17:38:29 +0100

    Increase motion granularity

--- terra/config.py
+++ terra/config.py
@@ -40,8 +40,8 @@ class ImmutableAgentConfig(NamedTuple):
     """
 
     dimensions: ExcavatorDims = ExcavatorDims()
-    angles_base: int = 8
-    angles_cabin: int = 8
+    angles_base: int = 12
+    angles_cabin: int = 12
     max_arm_extension: int = 1  # numbering starts from 0 (0 is the closest level)
 
 
--- terra/state.py
+++ terra/state.py
@@ -167,7 +167,7 @@ class State(NamedTuple):
         Use for the forward action.
         """
         # TODO: Do not hardcode - find a way around JIT compilation to provide dimensionality as constant
-        return jax.nn.one_hot(base_orientation, 8, dtype=IntLowDim)
+        return jax.nn.one_hot(base_orientation, 12, dtype=IntLowDim)
 
     def _base_orientation_to_one_hot_backwards(self, base_orientation: IntLowDim):
         """
@@ -176,7 +176,7 @@ class State(NamedTuple):
         """
         # Create a permutation matrix by shifting the identity
         # TODO: Do not hardcode - find a way around JIT compilation to provide dimensionality as constant
-        num_angles = 8
+        num_angles = 12
         fwd_to_bkwd_transformation = jnp.roll(jnp.eye(num_angles, dtype=IntLowDim), shift=num_angles // 2, axis=0)
         orientation_one_hot = self._base_orientation_to_one_hot_forward(base_orientation)
         return orientation_one_hot @ fwd_to_bkwd_transformation
@@ -282,7 +282,7 @@ class State(NamedTuple):
     def _move_on_orientation(self, orientation_vector: Array) -> "State":
         # Compute the xy delta for a forward move along that angle.
         # TODO: Do not hardcode - find a way around JIT compilation to provide dimensionality as constant
-        angles = jnp.linspace(0, 2 * jnp.pi, 8, endpoint=False)
+        angles = jnp.linspace(0, 2 * jnp.pi, 12, endpoint=False)
         angles = (2 * jnp.pi) - angles
         xy_delta = self.env_cfg.agent.move_tiles * jnp.stack([jnp.cos(angles), jnp.sin(angles)], axis=-1)
         delta_xy = orientation_vector @ xy_delta
