d6e5ca835cf3fa3fee9fe73668a2c878400884d9
gioelemo <gioelemo@ethz.ch>
2025-03-25 17:33:05 +0100

    add possible model choices (tested up to now)

--- terra/viz/main_llm.py
+++ terra/viz/main_llm.py
@@ -3,8 +3,8 @@ from .main_manual_llm import run_experiment
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run an LLM-based simulation experiment.")
-    parser.add_argument("--model_name", type=str, required=True, help="Name of the LLM model to use.")
-    parser.add_argument("--model_key", type=str, required=True, help="Name of the LLM model key to use.")
+    parser.add_argument("--model_name", type=str, required=True, choices=["gpt-4o", "gemini-1.5-flash-latest"], help="Name of the LLM model to use.")
+    parser.add_argument("--model_key", type=str, required=True, choices=["gpt4o", "gemini"], help="Name of the LLM model key to use.")
     parser.add_argument("--num_timesteps", type=int, default=100, help="Number of timesteps to run.")
 
     args = parser.parse_args()
