d2af10309281b2df1e3d828adcdc44cdfa6535b6
gioelemo <gioelemo@ethz.ch>
2025-04-10 10:57:28 +0200

    fix num_angles base for LLM version

--- terra/state.py
+++ terra/state.py
@@ -159,7 +159,8 @@ class State(NamedTuple):
         Use for the forward action.
         """
         # TODO: Do not hardcode - find a way around JIT compilation to provide dimensionality as constant
-        return jax.nn.one_hot(base_orientation, 12, dtype=IntLowDim)
+        #return jax.nn.one_hot(base_orientation, 12, dtype=IntLowDim)
+        return jax.nn.one_hot(base_orientation, 4, dtype=IntLowDim)
 
     def _base_orientation_to_one_hot_backwards(self, base_orientation: IntLowDim):
         """
@@ -168,7 +169,8 @@ class State(NamedTuple):
         """
         # Create a permutation matrix by shifting the identity
         # TODO: Do not hardcode - find a way around JIT compilation to provide dimensionality as constant
-        num_angles = 12
+        #num_angles = 12
+        num_angles = 4
         fwd_to_bkwd_transformation = jnp.roll(jnp.eye(num_angles, dtype=IntLowDim), shift=num_angles // 2, axis=0)
         orientation_one_hot = self._base_orientation_to_one_hot_forward(base_orientation)
         return orientation_one_hot @ fwd_to_bkwd_transformation
@@ -274,7 +276,9 @@ class State(NamedTuple):
     def _move_on_orientation(self, orientation_vector: Array) -> "State":
         # Compute the xy delta for a forward move along that angle.
         # TODO: Do not hardcode - find a way around JIT compilation to provide dimensionality as constant
-        angles = jnp.linspace(0, 2 * jnp.pi, 12, endpoint=False)
+        #angles = jnp.linspace(0, 2 * jnp.pi, 12, endpoint=False)
+        angles = jnp.linspace(0, 2 * jnp.pi, 4, endpoint=False)
+
         angles = (angles + (jnp.pi / 2)) % (2 * jnp.pi)
         xy_delta = self.env_cfg.agent.move_tiles * jnp.stack([jnp.cos(angles), jnp.sin(angles)], axis=-1)
         delta_xy = orientation_vector @ xy_delta
