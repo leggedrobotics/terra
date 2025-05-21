c8eb93645117ccd23181538ea3fba25cfd5b50ff
Stachq1 <spiasecki@student.ethz.ch>
2025-05-21 14:28:17 +0200

    Extend generate_relocations.py with obstacle and non-dumpable zone generation

--- terra/env_generation/generate_relocations.py
+++ terra/env_generation/generate_relocations.py
@@ -1,7 +1,11 @@
 import numpy as np
+import os
 
 from terra.env_generation.procedural_data import (
-    initialize_image
+    add_non_dumpables,
+    add_obstacles,
+    initialize_image,
+    save_or_display_image
 )
 
 from terra.env_generation.utils import color_dict
@@ -10,6 +14,7 @@ def add_dumpables(img, n_dump_min, n_dump_max, size_dump_min, size_dump_max):
     w, h = img.shape[:2]
     n_dump = 0
     n_dump_todo = np.random.randint(n_dump_min, n_dump_max + 1)
+    cumulative_mask = np.zeros_like(img[..., 0], dtype=bool)
 
     while n_dump < n_dump_todo:
         # Randomly determine the size of the dumping zone
@@ -20,9 +25,10 @@ def add_dumpables(img, n_dump_min, n_dump_max, size_dump_min, size_dump_max):
         y = np.random.randint(0, h - sizeoy)
         # Update the dumping zone layer
         img[x : x + sizeox, y : y + sizeoy] = np.array(color_dict["dumping"])
+        cumulative_mask[x : x + sizeox, y : y + sizeoy] = True
         n_dump += 1  # Increment the count of zones added
 
-    return img
+    return img, cumulative_mask
 
 def generate_relocations(
     n_imgs,
@@ -32,13 +38,23 @@ def generate_relocations(
     n_dump_max,
     size_dump_min,
     size_dump_max,
+    n_obs_min,
+    n_obs_max,
+    size_obstacle_min,
+    size_obstacle_max
+    n_nodump_min,
+    n_nodump_max,
+    size_nodump_min,
+    size_nodump_max,
 ):
+    package_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    save_folder = os.path.join(package_dir, "data", "terra", "foundations")
+
     i = 0
     while i < n_imgs:
         img = initialize_image(img_edge_min, img_edge_max, color_dict["neutral"])
-        img = add_dumpables(img, n_dump_min, n_dump_max, size_dump_min, size_dump_max)
+        img, cumulative_mask = add_dumpables(img, n_dump_min, n_dump_max, size_dump_min, size_dump_max)
+        occ, cumulative_mask = add_obstacles(img, cumulative_mask, n_obs_min, n_obs_max, size_obstacle_min, size_obstacle_max)
+        dmp, cumulative_mask = add_non_dumpables(img, occ, cumulative_mask, n_nodump_min, n_nodump_max, size_nodump_min, size_nodump_max)
         # TODO: Add spilled soil to the image
-        # TODO: Add non-dumpable zones to the image
-        # TODO: Add obstacles to the image
-        # TODO: Save the image to a file
-        
+        save_or_display_image(img, occ, dmp, {}, save_folder, i)
