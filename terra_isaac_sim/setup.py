a53b0af859d898e711eb7861d43b424f531755a8
Stachq1 <spiasecki@student.ethz.ch>
2025-06-08 20:19:13 +0200

    Do not install the test map

--- terra_isaac_sim/setup.py
+++ terra_isaac_sim/setup.py
@@ -14,8 +14,6 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'),
             glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'test_map'),
-            glob('test_map/*')),
     ],
     install_requires=[
         'setuptools',
